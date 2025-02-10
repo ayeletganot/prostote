931d65ec221187c5d4e00d2837d3763fd8c8fc5a
ayeletganot <ayeletganot140@gmail.com>
2025-02-10 20:54:46 +0200

    authentiaction added

---
+++ auth.ts
@@ -0,0 +1,94 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from "@/db/prisma";
+import { compareSync } from "bcrypt-ts-edge";
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+
+export const config = {
+  pages: {
+    signIn: "/Sign-in",
+    error: "Sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          type: "email",
+        },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        // Find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+        // Check if user exists and password is correct
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
+          // If password is correct, return user object
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        // If user doesn't exist or password is incorrect, return null
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      session.user.id = token.sub;
+      session.user.name = token.name; 
+      session.user.role = token.role;
+      
+      if (trigger === 'update') {
+        session.user.name = user.name
+      }
+      return session
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        // If user has no name, use email as their default name
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
+
+          // Update the user in the database with the new name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+
+      // Handle session updates (e.g., name change)
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name;
+      }
+
+      return token;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
